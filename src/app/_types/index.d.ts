582314a69b1f7655164ba605e13fed0d8449052b
Young <rhsiddlsidhd@nate.com>
2025-07-17 17:33:00 +0900

    initial commit

---
+++ src/app/_types/index.d.ts
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+declare global {
+  var mongooseCache: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+}
---
+++ src/app/_utils/mongodb.ts
@@ -0,0 +1,51 @@
+//MongoDB 연결
+
+import mongoose from "mongoose";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@new-invitation-cluster.8umdvcl.mongodb.net/?retryWrites=true&w=majority&appName=new-invitation-cluster`;
+
+if (!uri) {
+  throw new Error(`MongoDB URI를 확인해주세요.`);
+}
+
+let cached = global.mongooseCache;
+
+if (!cached) {
+  cached = global.mongooseCache = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export const dbConnect = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+      serverSelectionTimeoutMS: 5000,
+    };
+
+    cached.promise = mongoose.connect(uri, opts);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+
+    console.log("MongoDB Connected");
+    return cached.conn;
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error("MongoDB Connected Fail", e.message);
+    }
+    cached.promise = null;
+    throw e;
+  }
+};
+
+// 연결 상태 확인 함수
+export const isConnected = () => {
+  return mongoose.connection.readyState === 1;
+};
