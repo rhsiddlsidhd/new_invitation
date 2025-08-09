941a26c99ec99baf10de7e4121f31f62f1238746
Young <rhsiddlsidhd@nate.com>
2025-08-09 10:05:12 +0900

    scrollYProgressCenter 추가

--- src/components/molecules/PreviewVerticalSlider.tsx
+++ src/components/molecules/PreviewVerticalSlider.tsx
@@ -1,23 +1,48 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useEffect, useRef, useState } from "react";
+import { motion, MotionValue, useTransform } from "framer-motion";
 import Img from "../atoms/Img";
 import Card from "../atoms/Card";
+// relative w-[240px] overflow-hidden  p-2 max-sm:hidden max-sm:w-full
+const PreviewVerticalSlider = ({
+  scrollYProgress,
+}: {
+  scrollYProgress: MotionValue<number>;
+}) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [scrollY, setScrollY] = useState<number>(0);
 
-const PreviewVerticalSlider = () => {
+  const CARDHEIGHT = 300;
+
+  const y = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [0, CARDHEIGHT * 2 - scrollY],
+  );
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+    const height = containerRef.current.scrollHeight;
+    setScrollY(height);
+  }, []);
+
   return (
-    <div className="relative w-[240px] overflow-hidden p-2 max-sm:hidden max-sm:w-full">
-      <motion.div className="space-y-4">
+    <div className="sticky top-0 h-screen w-[240px] p-2 max-sm:hidden max-sm:w-full">
+      <motion.div ref={containerRef} className="" style={{ y }}>
         {Array.from({ length: 6 }, (_, i) => {
           return (
-            <Card key={i} className="h-[300px] w-full">
-              <div className="relative h-3/5 w-full">
-                <Img src="/marriage.jpg" />
-              </div>
-              <ul className="p-2">
-                <li className="text-lg font-semibold text-[#5B5A57]">title</li>
-                <li className="text-sm text-[#8E8E8A]">description</li>
-              </ul>
-            </Card>
+            <div key={i} className="h-[300px] w-full">
+              <Card className="h-full w-full">
+                <div className="relative h-3/5 w-full">
+                  <Img src="/marriage.jpg" />
+                </div>
+                <ul className="p-2">
+                  <li className="text-lg font-semibold text-[#5B5A57]">
+                    title
+                  </li>
+                  <li className="text-sm text-[#8E8E8A]">description</li>
+                </ul>
+              </Card>
+            </div>
           );
         })}
       </motion.div>
--- src/components/template/PreviewContainer.tsx
+++ src/components/template/PreviewContainer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { motion, useMotionValueEvent, useScroll } from "framer-motion";
 
 import PreviewHorizonalSlider from "../molecules/PreviewHorizonalSlider";
@@ -14,10 +14,25 @@ const PreviewContainer = () => {
     offset: ["start start", "end end"],
   });
 
+  const { scrollYProgress: scrollYProgressCenter } = useScroll({
+    target: containerRef,
+    offset: ["start start", "center end"],
+  });
+
+  const { scrollYProgress: scrollYProgressEnd } = useScroll({
+    target: containerRef,
+    offset: ["center end", "end end"],
+  });
+
+  useMotionValueEvent(scrollYProgressEnd, "change", (latest) =>
+    console.log("Scroll Y Progress:", latest),
+  );
+
+  // className="relative z-20 flex w-full max-sm:block max-sm:h-[200vh] sm:p-2"
   return (
     <motion.section
       initial={{ backgroundColor: "#E7E6E2" }}
-      className="relative z-20 flex w-full max-sm:block max-sm:h-[200vh] sm:p-2"
+      className="relative z-20 flex h-[500vh] w-full max-sm:block sm:p-2"
       ref={containerRef}
     >
       <div className="sticky top-0 flex-3/4 shrink grow overflow-hidden text-[4.5vw] max-sm:top-1/4 max-sm:flex max-sm:flex-col sm:h-screen">
@@ -29,7 +44,7 @@ const PreviewContainer = () => {
         </p>
         <PreviewHorizonalSlider scrollYProgress={scrollYProgress} />
       </div>
-      <PreviewVerticalSlider />
+      <PreviewVerticalSlider scrollYProgress={scrollYProgressCenter} />
     </motion.section>
   );
 };
