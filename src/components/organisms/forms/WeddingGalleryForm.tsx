7ffa500e6908046eeae9625a22f3693d64b6d094
young <rhsiddlsidhd@nate.com>
2025-08-24 16:20:15 +0900

    cloudinary storage create url after applicate

--- src/components/organisms/forms/WeddingGalleryForm.tsx
+++ src/components/organisms/forms/WeddingGalleryForm.tsx
@@ -8,6 +8,7 @@ import { useUserStore } from "@/store/userStore";
 import React, { useActionState, useEffect } from "react";
 
 const CLOUD_NAME = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
+const UPLOAD_PRESET = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET;
 
 const WeddingGalleryForm = () => {
   const [state, action, pending] = useActionState(updateInvitationInfo, null);
@@ -28,44 +29,53 @@ const WeddingGalleryForm = () => {
     // 클라이언트 > cloudinary 외부 스토리지로 Img 바로 저장
     e.preventDefault();
     const form = e.currentTarget;
+
     const formData = new FormData(form);
-    const galleries = new Map<string, { type: string; urls: File[] }>();
+    const galleries = new Map<
+      string,
+      { type: string; urls: (File | string)[] }
+    >();
 
     for (const [keys, value] of formData.entries()) {
-      if (typeof value === "string") {
-        const typeMatch = keys.match(/^gallery-([a-zA-Z0-9-]+)-type$/);
-        const id = typeMatch && typeMatch[1];
-        if (id) {
-          const gallery = galleries.has(id)
-            ? galleries.get(id)
-            : galleries.set(id, { type: "", urls: [] }).get(id);
-          gallery!.type = value;
-        }
+      const typeMatch = keys.match(/^gallery-([a-zA-Z0-9-]+?)(-type)?$/);
+      if (!typeMatch) continue;
+      const id = typeMatch && typeMatch[1];
+      const isType = Boolean(typeMatch[2]);
+
+      if (!galleries.has(id)) galleries.set(id, { type: "", urls: [] });
+
+      const gallery = galleries.get(id);
+
+      if (!gallery) continue;
+      if (!isType && value instanceof File) {
+        gallery.urls.push(value);
       } else {
-        const typeMatch = keys.match(/^gallery-([a-zA-Z0-9-]+)/);
-        const id = typeMatch && typeMatch[1];
-        if (id) {
-          const gallery = galleries.get(id);
-          console.log("value.name", value.name, value);
-          const res = await fetch(
-            `https://api.cloudinary.com/v2/${CLOUD_NAME}/image/upload`,
-            {
-              method: "POST",
-              body: value,
-            },
-          );
-          console.log("res===============>:", res);
-        }
+        gallery.type = value as string;
       }
     }
 
-    /**
-     * galleries = {
-     *   id: string;
-     *   type:string;
-     *   urls: [...url,url];
-     * }[]
-     */
+    for (const [id, gallery] of galleries) {
+      const uploadUrls: string[] = await Promise.all(
+        gallery.urls.map(async (file) => {
+          const formData = new FormData();
+          formData.append("file", file);
+          formData.append("upload_preset", `${UPLOAD_PRESET}`);
+          formData.append("folder", "galleryImg");
+          const res = await fetch(
+            `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
+            { method: "POST", body: formData },
+          );
+          const { url } = await res.json();
+          return url;
+        }),
+      );
+      const isGallery = galleries.get(id);
+      if (isGallery) {
+        gallery.urls = uploadUrls;
+      }
+    }
+
+    await action(new FormData(galleries));
   };
 
   return (
