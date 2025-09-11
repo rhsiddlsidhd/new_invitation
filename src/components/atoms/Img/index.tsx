e8a810d18e13ee5dacb5cab9dd877a71620d599e
Young <rhsiddlsidhd@nate.com>
2025-09-11 16:23:03 +0900

    cloudinary sample component img size resizing

--- src/components/atoms/Img/index.tsx
+++ src/components/atoms/Img/index.tsx
@@ -2,9 +2,16 @@ import Image, { StaticImageData } from "next/image";
 import React from "react";
 
 const Img = ({ src }: { src: string | StaticImageData }) => {
+  if (!src) return null;
+
+  const optimizedSrc =
+    typeof src === "string" && src.includes("res.cloudinary.com")
+      ? src.replace("/upload/", "/upload/f_auto,q_auto,w_800/")
+      : src;
+
   return (
     <Image
-      src={src}
+      src={optimizedSrc}
       fill
       alt={`예시 이미지`}
       className="object-cover"
--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Header = async () => {
   return (
-    <div className="bg-trasparent fixed bottom-0 z-50 w-full px-2 backdrop-blur-md max-sm:sticky max-sm:top-0">
+    <div className="bg-trasparent fixed bottom-0 z-50 h-fit w-full px-2 backdrop-blur-md max-sm:sticky max-sm:top-0">
       <div className="relative flex h-full w-full items-center justify-between">
         <Logo />
         <div className="max-sm:relative sm:absolute sm:left-1/2 sm:-translate-x-1/2">
