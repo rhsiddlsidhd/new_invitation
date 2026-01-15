f805f5106599c79dc74f05ab2336094670e5ba78
Youngjae Shin <rhsiddlsidhd@nate.com>
2026-01-15 21:49:40 +0900

    kako map error 수정

--- src/lib/kakao/useKakaoLoader.ts
+++ src/lib/kakao/useKakaoLoader.ts
@@ -1,7 +1,7 @@
 import { useKakaoLoader as useKakaoLoaderOrigin } from "react-kakao-maps-sdk";
 
 const useKakaoLoader = () => {
-  const apikey = process.env.KAKAO_MAP_API_KEY;
+  const apikey = process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY;
 
   if (!apikey) {
     throw new Error("카카오맵 API 키가 없습니다.");
